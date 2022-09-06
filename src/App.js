9ef9b5d668a7b8a6280e733da86c77fcaa743257
Victor <94798862+victorhe33@users.noreply.github.com>
2022-09-06 17:53:42 -0400

    tinkering with US timezones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 
-const App = () => {
+//DAYJS
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(timezone);
+dayjs.tz.setDefault("America/New_York");
+
+//https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+function App() {
   return (
-    <div className="App">
-      <h1>React with Webpack and Babel!</h1>
+    <div>
+      <Clock />
     </div>
-  )
+  );
 }
+
+function Clock() {
+  const date = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+    timeZoneName: "short",
+  })
+  const date2 = new Date().toLocaleTimeString("en-US", {
+    timeZone: "US/Central",
+    timeZoneName: "short",
+    hour12: true
+  })
+  const date3 = new Date().toLocaleTimeString("en-US", {
+    timeZone: "US/Mountain",
+    timeZoneName: "short",
+    hour12: true
+  })
+  const date4 = new Date().toLocaleTimeString("en-US", {
+    timeZone: "US/Pacific",
+    timeZoneName: "short",
+    hour12: true
+  })
+  const now = new dayjs();
+  const time = String(now);
+  return (
+    <div>
+      <h1>hello world!</h1>
+      <h2>EST: {date}</h2>
+      <h2>Central Time: {date2}</h2>
+      <h2>Mountain Time: {date3}</h2>
+      <h2>Pacific Time: {date2}</h2>
+      {/* <h1>Using the dayjs library...</h1> */}
+
+
+      <span>{time}</span>
+      <p>{dayjs().format()}</p>
+    </div>
+  );
+}
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function tick() {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+setInterval(tick, 1000);
+
+
