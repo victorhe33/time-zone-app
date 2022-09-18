2f66291f3d46482d441bb55861d74c5f578a4832
Victor <94798862+victorhe33@users.noreply.github.com>
2022-09-17 22:26:44 -0400

    refactoring - delete comments/old class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
-
 //DAYJS
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
@@ -13,36 +12,6 @@ dayjs.tz.setDefault("America/New_York");
 //https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
 //CLICK HANDLERS
-
-//CREATECLICK
-// async function createClick() {
-//   console.log('createClick');
-//   const selection = document.getElementById('selectTime').value
-//   console.log(selection)
-//   const response = await fetch('./api/', {
-//     method: 'POST',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ name: selection }),
-//   })
-//   const data = await response.json();
-//   console.log(data);
-
-  // const response = await fetch('./api/', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify({name: "Victor"}),
-  // })
-  // const data = await response.json();
-  // console.log(data);
-// }
-
-
 
 //UPDATECLICK FUNCTION
 async function updateClick() {
@@ -73,12 +42,10 @@ async function deleteClick() {
   console.log(data);
 }
 
-
 //REACT COMPONENT
 function App() {
   return (
     <div>
-      {/* <Clock /> */}
       <MiniClock/>
     </div>
   );
@@ -86,14 +53,6 @@ function App() {
 
 //test case of using state to rerender re: time
 const MiniClock = () => {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     date: new Date(),
-  //     timezones: [],
-  //   };
-  //   this.timezones = ["US/Eastern", "US/Central", "US/Mountain", "US/Pacific"]
-  // }
   const [date, setDate] = useState(new Date());
   const [timezones, setTimezones] = useState([]);
 
@@ -127,9 +86,9 @@ const MiniClock = () => {
   console.log('timezones', timezones);
   }
 
-
   const newTimezones = [];
   const timeComponents = [];
+
   //READCLICK FUNCTION
   async function readClick() {
     console.log('readClick');
@@ -165,11 +124,9 @@ const MiniClock = () => {
       <h2>
         {timeComponents}
       </h2>
-      <h2></h2>
 
     </div>
   )
-  
 }
 
 function Timezone (props) {
@@ -187,65 +144,5 @@ function Timezone (props) {
     </div>
   );
 }
-
-// for (let i = 0; i < this.state.urls.length; i++) {
-//   feedItems.push(<FeedItem url={this.state.urls[i]} key={`FeedItem${i}`} id={i} handleImage={this.handleImage} />)
-// }
-
-// return (
-//   <div style={styles.container} id="feed">
-//     {feedItems}
-//   </div>
-// );
-
-//19:45:35 AM CDT
-
-// function Clock() {
-//   // const test = new Date().parse();
-
-//   const utc = new Date().toUTCString();
-
-//   const date = new Date().toLocaleTimeString("en-US", {
-//     timeZone: "America/New_York",
-//     timeZoneName: "short",
-//   })
-//   const date2 = new Date().toLocaleTimeString("en-US", {
-//     timeZone: "US/Central",
-//     timeZoneName: "short",
-//     hour12: true
-//   })
-//   const date3 = new Date().toLocaleTimeString("en-US", {
-//     timeZone: "US/Mountain",
-//     timeZoneName: "short",
-//     hour12: true
-//   })
-//   const date4 = new Date().toLocaleTimeString("en-US", {
-//     timeZone: "US/Pacific",
-//     timeZoneName: "short",
-//     hour12: true
-//   })
-//   const now = new dayjs();
-//   const time = String(now);
-
-//   return (
-//     <div>
-//       <h1>hello world!</h1>
-//       <button onClick={createClick}>create</button>
-//       <button onClick={readClick}>read</button>
-//       <button onClick={updateClick}>update</button>
-//       <button onClick={deleteClick}>delete</button>
-//       <h2>UTC: {utc}</h2>
-//       <h2>EST: {date}</h2>
-//       <h2>Central Time: {date2}</h2>
-//       <h2>Mountain Time: {date3}</h2>
-//       <h2>Pacific Time: {date4}</h2>
-//       <h1>Using the dayjs library...</h1>
-//       <span>TIME: {time}</span>
-//       <p>{dayjs().format()}</p>
-//     </div>
-//   );
-// }
-
-
 
 export default App;
